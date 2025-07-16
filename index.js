2133bbf1b9f2f10d9fee467284fd35f37aa6f883
lolodev <horaciodev1706@gmail.com>
2025-07-16 18:29:18 -0300

    BoredBot - Extra Styling

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-const buttonEl = document.querySelector('button');
-const activityEl = document.querySelector('h4');
+const bodyEl = document.querySelector('body');
+const getActivityEl = document.querySelector('#get-activity');
+const activityEl = document.querySelector('#activity');
+const titleEl = document.querySelector('#title');
 
-buttonEl.addEventListener('click', () => {
+getActivityEl.addEventListener('click', () => {
     fetch("https://apis.scrimba.com/bored/api/activity")
         .then(response => response.json())
         .then(data => {
             activityEl.textContent = data.activity;
+            titleEl.textContent = "🦾 HappyBot🦿";
+            bodyEl.classList.add('fun');
         });
 });
