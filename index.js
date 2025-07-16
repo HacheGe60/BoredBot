68004842d817699d4952d2d74c42306430130a29
lolodev <horaciodev1706@gmail.com>
2025-07-16 18:17:27 -0300

    BoredBot JavaScript

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-// fetch("https://apis.scrimba.com/bored/api/activity")
-//     .then(response => response.json())
-//     .then(data => {
-//         console.log(data)
-//         document.getElementById("activity-name").textContent = data.activity
-//     })
+const buttonEl = document.querySelector('button');
+const activityEl = document.querySelector('h4');
+
+buttonEl.addEventListener('click', () => {
+    fetch("https://apis.scrimba.com/bored/api/activity")
+        .then(response => response.json())
+        .then(data => {
+            activityEl.textContent = data.activity;
+        });
+});
